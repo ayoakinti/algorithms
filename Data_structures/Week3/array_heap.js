51bbe68ffd2f90ed8b5911fbaafd129ba63c5e28
Ayomide Akintimehin <ayoakinti@gmail.com>
2020-11-18 09:53:51 +0100

    week3 2 questions

--- Data_structures/Week3/array_heap.js
+++ Data_structures/Week3/array_heap.js
@@ -16,122 +16,162 @@ rl.on("line", () => {
         // let res = "";
         // let res = result[1][i][0] + " " + result[1][i][1]
         // console.log(res);
-        console.log(...result[1][i]);
+        console.log(result[1][i]);
       }
     }
     rl.close();
   });
 });
 
+// function convertHeap(arr) {
+//   //   console.log(arr, "input");
+//   let swaps = [];
+//   let length = arr.length;
+//   let left = false;
+//   for (let i = length - 1; i > 0; i = i - 2) {
+//     // console.log(arr[i], i, "i");
+//     if (i % 2 != 0) {
+//       left = false;
+//       let parentindex = (i - 1) / 2;
+//       let parent = arr[parentindex];
+//       if (parent > arr[i]) {
+//         swaps.push([parentindex, i]);
+//         // swaps.push(parentindex + " " + i);
+//         let clone = arr[i];
+//         arr[i] = parent;
+//         arr[parentindex] = clone;
+//       }
+//     } else {
+//       let leftchild = arr[i - 1];
+//       let rightchild = arr[i];
+//       let parentindex = (i - 2) / 2;
+//       let parent = arr[parentindex];
+//       if (leftchild < rightchild) {
+//         if (parent > leftchild) {
+//           swaps.push([parentindex, i - 1]);
+//           // console.log("na me")
+//           let childindex = i - 1;
+//           //   swaps.push(parentindex + " " + childindex);
+//           let clone = arr[i - 1];
+//           arr[i - 1] = parent;
+//           arr[parentindex] = clone;
+//           if (2 * (i - 1) + 2 < length) {
+//             i = 2 * (i - 1) + 4;
+//             // console.log(i, "i after tweak");
+//           } else if (2 * (i - 1) + 1 < length) {
+//             i = 2 * (i - 1) + 3;
+//             left = true;
+//           }
+//           //   let newparentIndex = i-1;
+//           //   let newLeftChildIndex = (2*newparentIndex)+1;
+//           //   let newRightChildIndex = (2*newparentIndex)+2;
+//           //   let newLeftChild = arr[newLeftChildIndex];
+//           //   let newRightChild = arr[newRightChildIndex];
+//           //   if (newLeftChild < newRightChild) {
+//           //     if (parent > newLeftChild) {
+//           //       swaps.push([newparentIndex, newLeftChildIndex]);
+//           //       let clone = arr[newLeftChildIndex];
+//           //       arr[newLeftChildIndex] = parent;
+//           //       arr[newparentIndex] = clone;
+//           //     }
+//           //   } else {
+//           //     if (parent > newRightChild) {
+//           //       swaps.push([newparentIndex, newRightChildIndex]);
+//           //       let clone = arr[newRightChildIndex];
+//           //       arr[newRightChildIndex] = parent;
+//           //       arr[newparentIndex] = clone;
+//           //     }
+//           //   }
+//         }
+//       } else {
+//         if (parent > rightchild) {
+//         //   swaps.push(parentindex + " " + i);
+//           swaps.push([parentindex, i]);
+
+//           let clone = arr[i];
+//           arr[i] = parent;
+//           arr[parentindex] = clone;
+//           if (2 * i + 2 < length) {
+//             i = 2 * i + 4;
+//             // console.log(i, "i after tweak");
+//           } else if (2 * i + 1 < length) {
+//             //   console.log("less than left child");
+//             i = 2 * i + 3;
+//             left = true;
+//             // console.log(i, "i after tweak");
+//           }
+
+//           //   let newparentIndex = i;
+//           //   let newLeftChildIndex = (2*newparentIndex)+1;
+//           //   let newRightChildIndex = (2*newparentIndex)+2;
+//           //   let newLeftChild = arr[newLeftChildIndex];
+//           //   let newRightChild = arr[newRightChildIndex];
+//           //   if (newLeftChild < newRightChild) {
+//           //     if (parent > newLeftChild) {
+//           //       swaps.push([newparentIndex, newLeftChildIndex]);
+//           //       let clone = arr[newLeftChildIndex];
+//           //       arr[newLeftChildIndex] = parent;
+//           //       arr[newparentIndex] = clone;
+//           //     }
+//           //   } else {
+//           //     if (parent > newRightChild) {
+//           //       swaps.push([newparentIndex, newRightChildIndex]);
+//           //       let clone = arr[newRightChildIndex];
+//           //       arr[newRightChildIndex] = parent;
+//           //       arr[newparentIndex] = clone;
+//           //     }
+//           //   }
+//         }
+//       }
+//     }
+//     if (i % 2 != 0 && !left) {
+//       // console.log(left);
+//       i++;
+//     }
+//   }
+
+//   // console.log(arr, "arr");
+//   // console.log(swaps, "swaps");
+//   let swapslength = swaps.length;
+//   return [swapslength, swaps];
+// }
+
 function convertHeap(arr) {
-  //   console.log(arr, "input");
-  let swaps = [];
-  let length = arr.length;
-  let left = false;
-  for (let i = length - 1; i > 0; i = i - 2) {
-    // console.log(arr[i], i, "i");
-    if (i % 2 != 0) {
-      left = false;
-      let parentindex = (i - 1) / 2;
-      let parent = arr[parentindex];
-      if (parent > arr[i]) {
-        swaps.push([parentindex, i]);
-        // swaps.push(parentindex + " " + i);
-        let clone = arr[i];
-        arr[i] = parent;
-        arr[parentindex] = clone;
-      }
-    } else {
-      let leftchild = arr[i - 1];
-      let rightchild = arr[i];
-      let parentindex = (i - 2) / 2;
-      let parent = arr[parentindex];
-      if (leftchild < rightchild) {
-        if (parent > leftchild) {
-          swaps.push([parentindex, i - 1]);
-          // console.log("na me")
-          let childindex = i - 1;
-          //   swaps.push(parentindex + " " + childindex);
-          let clone = arr[i - 1];
-          arr[i - 1] = parent;
-          arr[parentindex] = clone;
-          if (2 * (i - 1) + 2 < length) {
-            i = 2 * (i - 1) + 4;
-            // console.log(i, "i after tweak");
-          } else if (2 * (i - 1) + 1 < length) {
-            i = 2 * (i - 1) + 3;
-            left = true;
-          }
-          //   let newparentIndex = i-1;
-          //   let newLeftChildIndex = (2*newparentIndex)+1;
-          //   let newRightChildIndex = (2*newparentIndex)+2;
-          //   let newLeftChild = arr[newLeftChildIndex];
-          //   let newRightChild = arr[newRightChildIndex];
-          //   if (newLeftChild < newRightChild) {
-          //     if (parent > newLeftChild) {
-          //       swaps.push([newparentIndex, newLeftChildIndex]);
-          //       let clone = arr[newLeftChildIndex];
-          //       arr[newLeftChildIndex] = parent;
-          //       arr[newparentIndex] = clone;
-          //     }
-          //   } else {
-          //     if (parent > newRightChild) {
-          //       swaps.push([newparentIndex, newRightChildIndex]);
-          //       let clone = arr[newRightChildIndex];
-          //       arr[newRightChildIndex] = parent;
-          //       arr[newparentIndex] = clone;
-          //     }
-          //   }
-        }
-      } else {
-        if (parent > rightchild) {
-        //   swaps.push(parentindex + " " + i);
-          swaps.push([parentindex, i]);
-
-          let clone = arr[i];
-          arr[i] = parent;
-          arr[parentindex] = clone;
-          if (2 * i + 2 < length) {
-            i = 2 * i + 4;
-            // console.log(i, "i after tweak");
-          } else if (2 * i + 1 < length) {
-            //   console.log("less than left child");
-            i = 2 * i + 3;
-            left = true;
-            // console.log(i, "i after tweak");
-          }
-
-          //   let newparentIndex = i;
-          //   let newLeftChildIndex = (2*newparentIndex)+1;
-          //   let newRightChildIndex = (2*newparentIndex)+2;
-          //   let newLeftChild = arr[newLeftChildIndex];
-          //   let newRightChild = arr[newRightChildIndex];
-          //   if (newLeftChild < newRightChild) {
-          //     if (parent > newLeftChild) {
-          //       swaps.push([newparentIndex, newLeftChildIndex]);
-          //       let clone = arr[newLeftChildIndex];
-          //       arr[newLeftChildIndex] = parent;
-          //       arr[newparentIndex] = clone;
-          //     }
-          //   } else {
-          //     if (parent > newRightChild) {
-          //       swaps.push([newparentIndex, newRightChildIndex]);
-          //       let clone = arr[newRightChildIndex];
-          //       arr[newRightChildIndex] = parent;
-          //       arr[newparentIndex] = clone;
-          //     }
-          //   }
-        }
-      }
-    }
-    if (i % 2 != 0 && !left) {
-      // console.log(left);
-      i++;
-    }
+  // console.log(arr, "input");
+  let n = Math.floor((arr.length) / 2) -1;
+  // console.log(n);
+  let output = [];
+  for(let i = n; i >= 0; i--){
+    // console.log(arr, i, "for loop");
+    siftDown(arr, i, output);
+    // console.log(arr, i, "for loop");
   }
+  // console.log(arr, "arr")
+  let length = output.length;
+  // console.log(length);
+  // console.log(output, "output")
+  return [length, output]
+  // return;
+}
 
-  // console.log(arr, "arr");
-  // console.log(swaps, "swaps");
-  let swapslength = swaps.length;
-  return [swapslength, swaps];
+function siftDown(arr, i, output) {
+  // console.log(i, "i");
+  let minIndex = i;
+  let leftIndex = 2*i + 1;
+  let rightIndex = 2*i + 2;
+  if(arr[rightIndex] && arr[rightIndex] < arr[minIndex]){
+    minIndex = rightIndex;
+  }
+  if(arr[leftIndex] && arr[leftIndex] < arr[minIndex]){
+    minIndex = leftIndex;
+  }
+  if(minIndex !== i){
+    let clone = arr[i];
+    // console.log(i, minIndex);
+    output.push(i + " "+ minIndex);
+    arr[i] = arr[minIndex];
+    arr[minIndex] = clone;
+    return siftDown(arr, minIndex, output);
+  }
+  // return;
 }
--- Data_structures/Week3/parallel_processing.js
+++ Data_structures/Week3/parallel_processing.js
@@ -11,12 +11,136 @@ rl.on("line", (line) => {
   rl.on("line", (line) => {
     const input = line.toString().split(" ").map(Number);
 
-    console.log(Processes(n, m, input));
-    
+    // console.log(parallelProcessing(n, m, input));
+
+    let result = parallelProcessing(n, m, input);
+    if (result.length) {
+      for (let i = 0; i < result.length; i++) {
+        // let res = "";
+        // let res = result[1][i][0] + " " + result[1][i][1]
+        // console.log(res);
+        console.log(result[i]);
+      }
+    }
+
     rl.close();
   });
 });
 
-function Processes(n, m, input) {
-  console.log(n, m, input, "inputs");
+function parallelProcessing(n, m, input) {
+  // console.log(n, m, input, "inputs");
+  let minHeap = [];
+  let output = [];
+  let j = 0;
+  for (let i = 0; i < m; i++) {
+    // console.log(i, "i");
+    if (i < n) {
+      insert(minHeap, [j, 0, input[i]]);
+      output.push(j + " " + 0);
+      j++;
+      // console.log(j, "j");
+
+      // console.log(threads, i, "i < n");
+      // console.log(output, i, "i < n");
+      // console.log(i, "i < n");
+    } else {
+      let oldElement = minHeap[0];
+      // console.log(oldElement, "oldElement");
+      let newElement = [];
+      newElement[0] = oldElement[0];
+      newElement[1] = oldElement[2];
+      newElement[2] = oldElement[2] + input[i];
+      // console.log(newElement, "newElement");
+      output.push(newElement[0] + " " + newElement[1]);
+      extractMin(minHeap);
+      insert(minHeap, newElement);
+    }
+  }
+  // console.log(minHeap, "minHeap");
+  // console.log(output, "output");
+  return output;
+}
+
+function siftDown(arr, i) {
+  // console.log("entered siftdown");
+  // console.log(i, "i");
+  let minIndex = i;
+  let leftIndex = 2 * i + 1;
+  let rightIndex = 2 * i + 2;
+  if (arr[rightIndex] && arr[rightIndex][2] <= arr[minIndex][2]) {
+    if (arr[rightIndex][2] == arr[minIndex][2]) {
+      if (arr[rightIndex][0] < arr[minIndex][0]) {
+        minIndex = rightIndex;
+      }
+    } else {
+      minIndex = rightIndex;
+    }
+  }
+  if (arr[leftIndex] && arr[leftIndex][2] <= arr[minIndex][2]) {
+    if (arr[leftIndex][2] == arr[minIndex][2]) {
+      if (arr[leftIndex][0] < arr[minIndex][0]) {
+        minIndex = leftIndex;
+      }
+    } else {
+      minIndex = leftIndex;
+    }
+  }
+  if (minIndex !== i) {
+    let clone = arr[i];
+    // console.log(i, minIndex);
+    arr[i] = arr[minIndex];
+    arr[minIndex] = clone;
+    return siftDown(arr, minIndex);
+  }
+}
+
+function siftUp(arr, i) {
+  // console.log(arr, i, "entered siftup");
+  let parentIndex;
+  if (i % 2 == 0) {
+    parentIndex = (i - 2) / 2;
+  } else {
+    parentIndex = (i - 1) / 2;
+  }
+  // console.log(parentIndex, "parentIndex");
+  if (arr[parentIndex][2] >= arr[i][2]) {
+    if (arr[parentIndex][2] == arr[i][2]) {
+      // console.log("equal times");
+      if (arr[parentIndex][0] > arr[i][0]) {
+        let clone = arr[parentIndex];
+        arr[parentIndex] = arr[i];
+        arr[i] = clone;
+        if(parentIndex != 0){
+          return siftUp(arr, parentIndex);
+        }
+      }
+    } else {
+      // console.log("different times");
+      let clone = arr[parentIndex];
+      arr[parentIndex] = arr[i];
+      arr[i] = clone;
+      if(parentIndex != 0){
+        return siftUp(arr, parentIndex);
+      }
+    }
+  }
+}
+
+function insert(arr, newElement) {
+  // console.log("entered insert");
+  if (arr.length < 1) {
+    arr.push(newElement);
+  } else {
+    arr.push(newElement);
+    let index = arr.length-1;
+    siftUp(arr, index);
+  }
+}
+
+function extractMin(arr) {
+  // console.log("entered extractMin");
+  let index = arr.length - 1;
+  arr[0] = arr[index];
+  arr.pop();
+  siftDown(arr, 0);
 }
